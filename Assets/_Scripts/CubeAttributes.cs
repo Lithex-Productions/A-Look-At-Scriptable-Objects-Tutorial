d7f2275a1f77d7ce4e3320c99852f8f8c1873e37
Brian Thex <brian.thex@gmail.com>
2021-02-04 13:47:29 -0600

    project upload

---
+++ Assets/_Scripts/CubeAttributes.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace LP.ScriptableObjectsTutorial
+{
+    [CreateAssetMenu(fileName = "CubeAttributes", menuName = "Cube Attributes")]
+    public class CubeAttributes : ScriptableObject
+    {
+        public string cubeName = string.Empty;
+        public float size = 0;
+        public Color color = new Color(0, 0, 0);
+    }
+}
+
---
+++ Assets/_Scripts/CubeController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace LP.ScriptableObjectsTutorial
+{
+    public class CubeController : MonoBehaviour
+    {
+        [SerializeField] private CubeAttributes Attributes = null;
+        [SerializeField] private Material mat = null;
+
+        private void Start()
+        {
+            gameObject.name = Attributes.cubeName;
+            transform.localScale = new Vector3(Attributes.size, Attributes.size, Attributes.size);
+            mat.color = Attributes.color;
+        }
+    }
+}
+
